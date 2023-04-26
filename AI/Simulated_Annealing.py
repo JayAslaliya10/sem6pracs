5b543e0280c7dce932e49d0f22a09bcb7ff561d8
Jay Aslaliya <87231346+JayAslaliya10@users.noreply.github.com>
2023-04-26 23:24:41 +0530

    Add files via upload

--- AI/Simulated_Annealing.py
+++ AI/Simulated_Annealing.py
@@ -71,3 +71,31 @@ for i in range(M):
 	for j in range(N):
 		row += sourceArray[i][j] + " "
 	print(row)
+
+
+
+# import random
+# import math
+# def objFunc(x):
+#     return math.sin(x)-math.cos(2*x)
+# def simulatedAnnealing(x, maxItr, T):
+#     fx = objFunc(x)
+#     Tmin = 0.0001
+#     for _ in range(maxItr):
+#         alpha = 0.99
+#         T = T*alpha
+#         dx = (random.random()*2-1)*T
+#         next_x = x+dx
+#         next_fx = objFunc(next_x)
+#         if next_fx > fx or random.random() < math.exp((next_fx-fx)/T):
+#             x = next_x
+#             fx = next_fx
+#         if T < Tmin:
+#             break
+#     return x
+# x=1
+# maxIter = 1000 
+# T = 1.0
+# res = simulatedAnnealing(x, maxIter, T)
+# # Print the results
+# print(f'Point found: {res}\nf(x): {objFunc(res)}')
